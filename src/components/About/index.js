a583194dadfbbc616fda2ea4cf90f1b866a46ac7
algotoday <100335717+algotoday@users.noreply.github.com>
2022-08-30 17:07:08 -0700

    mod profile

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,11 +12,14 @@ function About() {
          as well as other experiential efficacies that I have gained from both the Private and Tech sectors...
          <br>
          Having recently graduated from UCLA's Full Stack Web Development Extended Program - I am 
-         now certified and proficient in both Front/Back-End and various Dev Tools / Database applications.
+         now certified and proficient in both Front/Back-End as well as various Dev Tools and Database applications.
          
          </br>
+         I offer broad organizational perspective and results-driven approach to projects, products and strategies 
+         with a focus on mobile-first and responsive web design.
          
-         
+         Resolved to forging new ground in many areas, including, but not limited to: UI and UX for the end user -
+         "Always bearing in mind that for every problem, there is always more than on solution..."
          
          
          </p>
