5a1517d9cc2d5fd298657bd92fb0af30f4b86ac9
algotoday <100335717+algotoday@users.noreply.github.com>
2022-08-30 18:20:24 -0700

    bug issue

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,22 +14,15 @@ function About() {
          Having recently graduated from UCLA's Full Stack Web Development Extended Program - I am 
          now certified in JavaScript, CSS, React.js, etc. and proficient in both Front/Back-End 
          as well as various Dev Tools and Database applications.
-<<<<<<< HEAD
-         
-=======
->>>>>>> 56f1e57fae57381d4184f32e4692b3625a991fcd
          </br>
          I offer broad organizational perspective and results-driven approach to projects, products and strategies 
          with a focus on mobile-first and responsive web design.
          
          Resolved to forging new ground in many areas, including, but not limited to: UI and UX for the end user -
-<<<<<<< HEAD
+
          
          "Always bearing in mind that for every problem, there is always more than on solution...".
-=======
 
-         "Always bearing in mind that for every problem, there is always more than on solution..."
->>>>>>> 56f1e57fae57381d4184f32e4692b3625a991fcd
          
          </p>
     </section>
