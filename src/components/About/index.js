aa81388032ba6f48b2264ccb5c7d6e2dfb5bc9f5
algotoday <100335717+algotoday@users.noreply.github.com>
2022-08-30 17:30:26 -0700

    changed brand statement

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,19 +7,20 @@ function About() {
       <h1 id="about">I'm A...</h1>
       <br>
       </br>
-      <p>Software Engineer who retains diverse attributes such as: 
+      <p> <p>Software Engineer who retains diverse attributes such as: 
          Financial Analysis, Project Management, Content Development/Strategy and Digital Marketing, 
          as well as other experiential efficacies that I have gained from both the Private and Tech sectors...
          <br>
          Having recently graduated from UCLA's Full Stack Web Development Extended Program - I am 
-         now certified and proficient in both Front/Back-End as well as various Dev Tools and Database applications.
-         
+         now certified in JavaScript, CSS, React.js, etc. and proficient in both Front/Back-End 
+         as well as various Dev Tools and Database applications.
          </br>
          I offer broad organizational perspective and results-driven approach to projects, products and strategies 
          with a focus on mobile-first and responsive web design.
          
          Resolved to forging new ground in many areas, including, but not limited to: UI and UX for the end user -
-         "Always bearing in mind that for every problem, there is always more than on solution...".
+
+         "Always bearing in mind that for every problem, there is always more than on solution..."
          
          </p>
     </section>
